5a97f15e4cabc5857eddd23548569ccc09474866
vanshita shah <vanshita.s@simformsolutions.com>
2024-09-23 12:10:42 +0530

    fix: build error in pre-rendering image-gallery page

--- app/image-gallery/page.tsx
+++ app/image-gallery/page.tsx
@@ -1,26 +1,36 @@
+import Image from 'next/image';
 import React from 'react';
 
 const ImageGallery = async () => {
   const fetchImages = async () => {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/images`, {
-      next: {
-        tags: ["images"]
-      }
-    });
-    const data = await response.json();
-    return data.imageUrls;
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/api/images`,
+        {
+          next: {
+            tags: ['images'],
+          },
+        }
+      );
+      const data = await response.json();
+      return data.imageUrls;
+    } catch (error) {
+      return [];
+    }
   };
 
   const images = await fetchImages();
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <h1 className="text-4xl font-bold text-center mb-8 text-gray-800">Image Gallery</h1>
+      <h1 className="text-4xl font-bold text-center mb-8 text-gray-800">
+        Image Gallery
+      </h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
         {images?.length > 0 ? (
           images.map((image, index) => (
             <div key={index} className="flex justify-center">
-              <img
+              <Image
                 src={image}
                 alt={`Image ${index + 1}`}
                 className="w-full h-64 object-contain rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300"
@@ -28,7 +38,9 @@ const ImageGallery = async () => {
             </div>
           ))
         ) : (
-          <p className="col-span-full text-center text-xl text-gray-600">No images found</p>
+          <p className="col-span-full text-center text-xl text-gray-600">
+            No images found
+          </p>
         )}
       </div>
     </div>
